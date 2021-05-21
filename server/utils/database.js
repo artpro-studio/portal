37be1fe171fefbdf6040bf81a43e02f891fd46ab
andreyi96 <ya.andreyi96@yandex.ru>
2021-05-21 18:43:44 +0300

    files

--- server/utils/database.js
+++ server/utils/database.js
@@ -10,24 +10,7 @@ const sequelize = new Sequelize('portal', 'postgres', '', {
   }
 });
 
-// //Prod
-// const sequelize = new Sequelize('portal', 'admin', '4inF0@adMIn25', {
-// const sequelize = new Sequelize('portal', 'admin', 'admin25', {
-//   host: 'localhost',
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     encrypt: true
-//   }
-// });
 
-// //Prod
-// const sequelize = new Sequelize('portal', 'admin', '4inF0@adMIn25', {
-//   host: 'localhost',
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     encrypt: true
-//   }
-// });
 
 
 module.exports = sequelize
